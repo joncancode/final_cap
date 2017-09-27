14c2dfcf8240118afcda672a919803dad4074047
daviddinnison <david.g.dinnison@gmail.com>
2017-09-27 00:57:27 -0400

    table and misc positioning

--- client/src/components/ProductWindow.js
+++ client/src/components/ProductWindow.js
@@ -15,26 +15,26 @@ class ProductWindow extends React.Component {
                 </div>
                 <div className="item-info">
                     <table>
-                    <tr>
-                        <th>Price</th>
-                        <th></th>
-                        <th>$50</th>
-                    </tr>
-                    <tr>
-                        <th>Code</th>
-                        <th></th>
-                        <th>1236786876876123</th>
-                    </tr>
-                    <tr>
-                        <th>Store 1</th>
-                        <th>Target</th>
-                        <th>Not in stock</th>
-                    </tr>
-                    <tr>
-                        <th>Store 2</th>
-                        <th>Gas station on Main St</th>
-                        <th>Last seen by Dan</th>
-                    </tr>
+                        <tr>
+                            <th>Price</th>
+                            <th>$50</th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <th>UPC Code</th>
+                            <th>21345515093</th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <th>Store 1</th>
+                            <th>Target</th>
+                            <th>Not in stock</th>
+                        </tr>
+                        <tr>
+                            <th>Store 2</th>
+                            <th>Gas station on Main St</th>
+                            <th>Last seen by Dan</th>
+                        </tr>
                     </table>
                 </div>
             </div>
