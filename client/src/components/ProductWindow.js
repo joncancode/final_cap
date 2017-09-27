c1202bf3b2ba3588d13db5c0b2e48899d51ec666
daviddinnison <david.g.dinnison@gmail.com>
2017-09-27 00:13:57 -0400

    basic ProductWindow table layout

--- client/src/components/ProductWindow.js
+++ client/src/components/ProductWindow.js
@@ -8,7 +8,35 @@ class ProductWindow extends React.Component {
     render() {
         return (
             <div className="product-window">
-                <p>this is the ProductWindow</p>
+                <div className="item-overview">
+                    <h2>Coke</h2>
+                    <p>Added by Jon</p>
+                    <img src="http://via.placeholder.com/350x150"/>
+                </div>
+                <div className="item-info">
+                    <table>
+                    <tr>
+                        <th>Price</th>
+                        <th></th>
+                        <th>$50</th>
+                    </tr>
+                    <tr>
+                        <th>Code</th>
+                        <th></th>
+                        <th>1236786876876123</th>
+                    </tr>
+                    <tr>
+                        <th>Store 1</th>
+                        <th>Target</th>
+                        <th>Not in stock</th>
+                    </tr>
+                    <tr>
+                        <th>Store 2</th>
+                        <th>Gas station on Main St</th>
+                        <th>Last seen by Dan</th>
+                    </tr>
+                    </table>
+                </div>
             </div>
         )
     }
