a140a7155bc9c4083b6ecf9a417e837ba38dddc3
dankato <apearmor@gmail.com>
2017-09-27 21:54:16 -0700

    seperated passport and routes from server index file

---
+++ server/Routes/authRoutes.js
@@ -0,0 +1,50 @@
+"use strict";
+const passport = require("passport");
+const { Users } = require('../Models/User');
+const { Products } = require('../Models/Product');
+
+module.exports = app => {
+
+    app.get('/api/auth/google',
+    passport.authenticate('google', {scope: ['profile']}));
+
+    app.get('/api/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/',
+        session: false
+    }),
+    (req, res) => {
+        res.cookie('accessToken', req.user.accessToken, {expires: 0});
+        res.redirect('/');
+    }
+    );
+
+    app.get('/api/auth/logout', (req, res) => {
+    req.logout();
+    res.clearCookie('accessToken');
+    res.redirect('/');
+    });
+
+
+    app.get('/api/questions',
+    passport.authenticate('bearer', {session: false}),
+    (req, res) => res.json(['Question 1', 'Question 2'])
+    );
+
+
+    app.post('/api/post', (req, res) => {
+    Products.create({
+        name: req.body.name
+
+    })
+    .then(()=> {
+        res.status(201).json(req.body);
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    });
+    });
+
+
+};
