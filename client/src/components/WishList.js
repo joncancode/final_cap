f9c904730dfba0eb43e08827203e8035dc25fb9f
daviddinnison <david.g.dinnison@gmail.com>
2017-09-28 16:21:53 -0400

    mid second popover view for wishlist add component

--- client/src/components/WishList.js
+++ client/src/components/WishList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {Router, Route} from 'react-router';
 
-var Modal = require('boron/DropModal');
+var Modal = require('boron/ScaleModal');
 
 import './styles/WishList.css';
 
@@ -19,6 +19,8 @@ class WishList extends React.Component {
         console.log('yung');
     }
 
+
+
     render() {
         const wishListItems = this.props.itemData.map((item, index) =>
         <li key={index}>
@@ -30,10 +32,21 @@ class WishList extends React.Component {
             <div className="wish-list">
                 <h2>Wish list</h2>
                 <div>
-                    <button onClick={this.showModal.bind(this)}>Open</button>
+                    <button onClick={this.showModal.bind(this)}>Add new item</button>
                         <Modal ref="modal" keyboard={this.callback}>
-                            <h2>I am a dialog</h2>
-                            <button onClick={this.hideModal.bind(this)}>Close</button>
+                            <div className="add-item-modal">
+                                <h2>Add new item</h2>
+                                    <input type="text"/>
+                                    <button onClick={this.showModal.bind(this)}>Search</button>
+                                    {/* <Modal ref="modal" keyboard={this.callback}>
+                                        <div className="add-item-modal">
+                                            <h2>something else</h2>
+                                            <button onClick={this.hideModal.bind(this)} className="close-button">Close</button>
+                                        </div>
+                                    </Modal> */}
+                                    <br/>
+                                <button onClick={this.hideModal.bind(this)} className="close-button">Close</button>
+                            </div>
                         </Modal>
                 </div>
                 <ul>
