f00662a253f8307c7e1b5bbc908332e18cf9018a
dankato <apearmor@gmail.com>
2017-09-27 21:56:03 -0700

    lowercased models folder

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -1,7 +1,7 @@
 "use strict";
 const passport = require("passport");
-const { Users } = require('../Models/User');
-const { Products } = require('../Models/Product');
+const { Users } = require('../models/User');
+const { Products } = require('../models/Product');
 
 module.exports = app => {
 
