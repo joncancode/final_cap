9bbb07e5a443cc12c59c71ae21c9fb3ac6f74c0e
dankato <apearmor@gmail.com>
2017-10-11 20:21:25 -0700

    minor updates to login page

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -5,7 +5,7 @@ export default function LoginPage() {
     return (
         <div id="LoginPageContainer" style={{ textAlign: "center" }}>
             <h2 id="header-title">Welcome to best app on Earth!</h2>  
-            <p id="description">Find Smithy is an application that brings the community together. Not sure if that <i>Nintendo Switch</i> is still available at the local Walmart? Add what you want to our communal list and allow the crowd sourcing to do its magic.</p>
+            <p id="description">Find Smithy is an application that brings the community together. Not sure if that <i>Nintendo Switch</i> is still available at the local <i>Walmart</i>? Add what you want to our communal list and allow the crowd sourcing to do its magic. Check it out by logging in with Google!</p>
         </div>
     )
 }   
